815dd16fcf0a41253de2dd0d88fce85b13e6a69c
Gabriel Dev <52919850+GabrielDev01@users.noreply.github.com>
2020-08-11 15:11:18 -0300

    Add files via upload
    Scripts utilizados na aula: "ABRINDO BAÚ com CHAVE na Unity e C#! | Tutorial de Iniciante - Desenvolvendo Jogos"

---
+++ Tutorial Abrindo Baú com Chave na Unity - Scripts/Bau.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bau : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if(FindObjectOfType<GameManager>().possuiChave == true)
+            {
+                Debug.Log("O baú foi aberto");
+            }
+            else
+            {
+                Debug.Log("Você precisa de uma Chave");
+            }
+        }
+    }
+}
